a7906b6974dd1ea85ffb68077fb8c65360159035
bruno1911 <mr.xxxx.bank.band@gmail.com>
2022-01-07 12:35:37 +0900

    背景色変更

--- AppleWatchAppSample WatchKit Extension/ContentView.swift
+++ AppleWatchAppSample WatchKit Extension/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
+            Color.green.ignoresSafeArea()
             Button(action: {
                 self.logStarting.toggle()
                 
